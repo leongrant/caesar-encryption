a78da178e7cf39a5b74ea27a1b5743d22cc7e9dc
L. I. Grant <leon.g_@outlook.com>
2019-11-13 16:37:11 +0100

    Update Caesar.java

--- Caesar.java
+++ Caesar.java
@@ -1,8 +1,22 @@
+// Caesar-Verschluesselung v1 |  @grantleo |   GdP Uebung 2
+import java.util.ArrayList;
+
 class Caesar
 {
-		public static void main(String[] args)
+	public static void main (String[] args)
+	{
+		int argsCount = args.length;
+
+		String[] inputs;
+
+		for(int i=0; i < args.length; i++)
 		{
-			System.out.println("test");
-			return;
+			inputs.add(args[i]);
 		}
+
+		System.out.println(inputs);
+
+		//char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		//char[] alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	}
 }
